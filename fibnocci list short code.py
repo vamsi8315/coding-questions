d20168a5f78c6a8b534ab01b6231d81f1e272682
vamsi8315 <52969534+vamsi8315@users.noreply.github.com>
2020-07-20 20:51:05 +0530

    Update fibnocci list short code.py

--- fibnocci list short code.py
+++ fibnocci list short code.py
@@ -7,3 +7,24 @@ for i in range(2,100):
 print(f)
 print(time.process_time())
              
+#next version of the code below
+import time
+def fib(n):
+    f=[0]*n
+    f[0]=0
+    f[1]=1
+    if(n>1):
+        for i in range(2,n):
+            f[i]=f[i-1]+f[i-2]
+        return(f)
+    return(f)
+l=int(input())
+if(l==0):
+    print("0")
+elif(l==1):
+    print("1")
+else:
+    x=fib(l+1)
+    print(x[l])
+
+
